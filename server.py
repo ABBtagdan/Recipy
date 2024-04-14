0f50f29239a545cd4f4fbd84cb25d2cb8bf0694b
abbTagDan <tage.danielsson@hitachigymnasiet.se>
2024-04-14 19:42:03 +0200

    added filtering to site

--- server.py
+++ server.py
@@ -1,16 +1,29 @@
 from flask import Flask, request, render_template
+from markupsafe import escape
 import data_loader
+import tools
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/recipes/", methods=['POST'])
+@app.route("/recipes/", methods=['GET'])
 def get_recipies():
-    x = data_loader.load("./recipes/"+request.form['user']+".json")
-    return x
- 
+    x = data_loader.load("./recipes/"+request.args['user']+".json")
+    filter = request.args.getlist('filter')
+    x = tools.filter(x, filter)
+    return render_template("recipes.html", recipes=x)
+
+@app.route("/filter_card", methods=['GET', 'DELETE'])
+def filter_card():
+    if request.method == "GET": 
+        filter: str = request.args['filter']
+        filter = filter.strip()
+        return f'<li hx-trigger="click" hx-target="this" hx-delete="/filter_card" hx-swap="outerHTML"><input type="hidden" name="filter" value="{escape(filter)}"/>{escape(filter)}</li>'
+    return ''
+
+if __name__ == "__main__":
+    app.run(debug=True)
